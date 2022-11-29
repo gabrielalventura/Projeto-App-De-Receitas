bab3fffa1d3e9ba7cd0f6122f6f3d69daf368781
Wellington Barbosa de Carvalho <104403502+WellingtonBarbosaDeCarvalho@users.noreply.github.com>
2022-11-29 18:03:05 -0300

    upload req-19

--- src/Pages/Meals.js
+++ src/Pages/Meals.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import useFetch from '../hooks/useFetch';
 
 function Meals() {
+  const { data } = useFetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+  console.log(data);
   return (
     <div>Meals</div>
   );
