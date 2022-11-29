a4e50615c2a0ba9653fb92f9b2a8a61025d4fdac
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-11-29 18:55:28 -0300

    Feat: Implementado inputs de login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import AppContext from '../context/AppContext';
 
 function Login() {
+  const { user } = useContext(AppContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    switch (name) {
+      case 'email':
+        console.log('acessou email')
+        setEmail(value)
+        break;
+      case 'password':
+        console.log('acessou password')
+        setPassword(value)
+        break;
+      default:
+        break;
+    }
+  };
+  
+
   return (
-    <div>Login</div>
+    <div>
+      <form>
+        <input name="email" type="email" data-testid="email-input" value={ email } onChange={ handleChange } />
+        <input name="password" type="password" data-testid="password-input" value={ password } onChange={ handleChange } />
+        <button type="button" data-testid="login-submit-btn">Enter</button>
+      </form>
+    </div>
   );
 }
 
