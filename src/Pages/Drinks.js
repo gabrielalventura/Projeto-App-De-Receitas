5e87690a861bedec28ddd1c3c6bdff599e42583e
Wellington Barbosa de Carvalho <104403502+WellingtonBarbosaDeCarvalho@users.noreply.github.com>
2022-11-29 19:23:01 -0300

    Update Drinks.js

--- src/Pages/Drinks.js
+++ src/Pages/Drinks.js
@@ -3,30 +3,30 @@ import useFetch from '../hooks/useFetch';
 import Header from '../components/Header';
 
 function Drinks() {
-  const { data: { drinks } } = useFetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+  const {
+    data: { drinks },
+  } = useFetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
   return (
     <div>
-    <Header title="Drinks" />
-        Drinks
-      { drinks !== undefined && drinks.map((drink, key) => {
-        const twelve = 12;
-        if (key < twelve) {
-          return (
-            <div
-              key={ key }
-              data-testid={ `${key}-recipe-card` }
-            >
-              <p data-testid={ `${key}-card-name` }>{drink.strDrink}</p>
-              <img
-                src={ drink.strDrinkThumb }
-                alt={ `receita do prato ${drink.strDrink}` }
-                data-testid={ `${key}-card-img` }
-              />
-            </div>
-          );
-        }
-        return undefined;
-      }) }
+      <Header title="Drinks" />
+      Drinks
+      {drinks !== undefined
+        && drinks.map((drink, key) => {
+          const twelve = 12;
+          if (key < twelve) {
+            return (
+              <div key={ key } data-testid={ `${key}-recipe-card` }>
+                <p data-testid={ `${key}-card-name` }>{drink.strDrink}</p>
+                <img
+                  src={ drink.strDrinkThumb }
+                  alt={ `receita do prato ${drink.strDrink}` }
+                  data-testid={ `${key}-card-img` }
+                />
+              </div>
+            );
+          }
+          return undefined;
+        })}
     </div>
   );
 }
