fd53b79c1f627083ab215b376f35586ada5abb19
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2022-11-29 18:36:23 -0300

    adiciona botões na página Perfil e renderiza email vindo do localStorage

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,8 +1,34 @@
 import React from 'react';
 
 function Profile() {
+  const userEmail = localStorage.getItem('user'); // userEmail é uma chave hipótetica, checar como o login será feito
   return (
-    <div>Profile</div>
+    <div>
+      <div>Profile</div>
+      <h2 data-testid="profile-email">{userEmail}</h2>
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+        // onClick={ handleDoneRecipes }
+      >
+        Done Recipes
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        type="button"
+        // onClick={ handleFavoriteRecipes }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        type="button"
+        // onClick={ handleLogout }
+      >
+        Logout
+      </button>
+    </div>
+
   );
 }
 
