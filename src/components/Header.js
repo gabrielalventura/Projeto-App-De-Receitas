04c0b8dbe4e6c53664bd441849ca64dd29622337
Arthur Medeiros <arthut.rex@gmail.com>
2022-11-29 18:40:40 -0300

    redirecionamento para pag profile e helper de testes adicionado

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header({ title }) {
   return (
     <nav>
-      <img
-        alt="profileIcon"
-        data-testid="profile-top-btn"
-        src={ profileIcon }
-      />
+      <Link to="/profile">
+        <img
+          alt="profileIcon"
+          data-testid="profile-top-btn"
+          src={ profileIcon }
+        />
+      </Link>
       {
         (title === 'Meals' || title === 'Drinks')
         && (
