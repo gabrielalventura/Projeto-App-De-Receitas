06907600bc97ac7b829b379f1c629d855bd99068
Wellington Barbosa de Carvalho <104403502+WellingtonBarbosaDeCarvalho@users.noreply.github.com>
2022-11-29 18:38:25 -0300

    req-19 complete

--- src/Pages/Drinks.js
+++ src/Pages/Drinks.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import useFetch from '../hooks/useFetch';
 
 function Drinks() {
+  const { data: { drinks } } = useFetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
   return (
-    <div>Drinks</div>
+    <div>
+      { drinks !== undefined && drinks.map((drink, key) => {
+        const twelve = 12;
+        if (key < twelve) {
+          return (
+            <div
+              key={ key }
+              data-testid={ `${key}-recipe-card` }
+            >
+              <p data-testid={ `${key}-card-name` }>{drink.strDrink}</p>
+              <img
+                src={ drink.strDrinkThumb }
+                alt={ `receita do prato ${drink.strDrink}` }
+                data-testid={ `${key}-card-img` }
+              />
+            </div>
+          );
+        }
+        return undefined;
+      }) }
+    </div>
   );
 }
 
--- src/Pages/Meals.js
+++ src/Pages/Meals.js
@@ -2,10 +2,29 @@ import React from 'react';
 import useFetch from '../hooks/useFetch';
 
 function Meals() {
-  const { data } = useFetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
-  console.log(data);
+  const { data: { meals } } = useFetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+  console.log(meals);
   return (
-    <div>Meals</div>
+    <div>
+      { meals !== undefined && meals.map((meal, key) => {
+        const twelve = 12;
+        if (key < twelve) {
+          return (
+            <div
+              key={ key }
+              data-testid={ `${key}-recipe-card` }
+            >
+              <p data-testid={ `${key}-card-name` }>{meal.strMeal}</p>
+              <img
+                src={ meal.strMealThumb }
+                data-testid={ `${key}-card-img` }
+                alt={ `receita do prato ${meal.strMeal}` }
+              />
+            </div>
+          );
+        } return undefined;
+      }) }
+    </div>
   );
 }
 
