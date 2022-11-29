0aa50cfa1df7eac23da5b1ca4ce15ffce496cfd9
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-11-29 19:03:07 -0300

    Fix: corrigindo erros de Lint

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,33 +1,42 @@
-import React, { useContext, useState } from 'react';
-import AppContext from '../context/AppContext';
+import React, { useState } from 'react';
+// import AppContext from '../context/AppContext';
 
 function Login() {
-  const { user } = useContext(AppContext);
+  // const { user } = useContext(AppContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleChange = ({ target }) => {
     const { name, value } = target;
     switch (name) {
-      case 'email':
-        console.log('acessou email')
-        setEmail(value)
-        break;
-      case 'password':
-        console.log('acessou password')
-        setPassword(value)
-        break;
-      default:
-        break;
+    case 'email':
+      setEmail(value);
+      break;
+    case 'password':
+      setPassword(value);
+      break;
+    default:
+      break;
     }
   };
-  
 
   return (
     <div>
       <form>
-        <input name="email" type="email" data-testid="email-input" value={ email } onChange={ handleChange } />
-        <input name="password" type="password" data-testid="password-input" value={ password } onChange={ handleChange } />
+        <input
+          name="email"
+          type="email"
+          data-testid="email-input"
+          value={ email }
+          onChange={ handleChange }
+        />
+        <input
+          name="password"
+          type="password"
+          data-testid="password-input"
+          value={ password }
+          onChange={ handleChange }
+        />
         <button type="button" data-testid="login-submit-btn">Enter</button>
       </form>
     </div>
