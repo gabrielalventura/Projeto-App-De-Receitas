4ef5abd267c559baa5f29bf5c94bb94a6053c879
Arthur Medeiros <arthut.rex@gmail.com>
2022-11-29 19:55:49 -0300

    botão de pesquisa implementado

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -1,9 +1,16 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import AppContext from './AppContext';
 
 function AppProvider({ children }) {
-  const values = useMemo(() => ({ name: '' }), []);
+  const [isActive, setIsActive] = useState(false);
+
+  const srchToggle = useCallback(() => {
+    setIsActive(isActive === false);
+  }, [isActive]);
+
+  const values = useMemo(() => (
+    { name: '', isSrchActive: isActive, srchToggle }), [isActive, srchToggle]);
 
   return (
     <AppContext.Provider value={ values }>
