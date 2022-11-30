517120223dff9da4ccf0d56b3cf21005857fb7da
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2022-11-30 17:50:17 -0300

    resolução erros do lint

--- src/Pages/FavoriteRecipes.js
+++ src/Pages/FavoriteRecipes.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';git
+import React from 'react';
 import Header from '../components/Header';
 
 function FavoriteRecipes() {
