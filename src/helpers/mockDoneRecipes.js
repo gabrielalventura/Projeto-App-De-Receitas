75dbe97ce27947320d1b52208e0296877968f959
Gabriela Ventura <79942730+gabrielalventura@users.noreply.github.com>
2022-12-01 17:45:23 -0300

    Revert "[Grupo 04] Requisitos 45 e 46"

--- src/helpers/mockDoneRecipes.js
+++ src/helpers/mockDoneRecipes.js
@@ -1,26 +1,13 @@
-const doneRecipes = [
-  {
-    id: '52771',
-    type: 'meal',
-    nationality: 'Italian',
-    category: 'Vegetarian',
-    alcoholicOrNot: '',
-    name: 'Spicy Arrabiata Penne',
-    image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-    doneDate: '23/06/2020',
-    tags: ['Pasta', 'Curry'],
-  },
-  {
-    id: '178319',
-    type: 'drink',
-    nationality: '',
-    category: 'Cocktail',
-    alcoholicOrNot: 'Alcoholic',
-    name: 'Aquamarine',
-    image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-    doneDate: '23/06/2020',
-    tags: [],
-  },
-];
+const doneRecipes = [{
+  id: '123',
+  type: 'meal',
+  nationality: 'brasil',
+  category: 'teste',
+  alcoholicOrNot: '',
+  name: 'tropeiro',
+  image: 'imagem',
+  doneDate: 'data',
+  tags: ['1', 'arroz', 'feijão'],
+}];
 
 export default doneRecipes;
