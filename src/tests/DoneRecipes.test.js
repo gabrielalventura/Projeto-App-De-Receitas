f29e2700348e6bfae674aa9d8bcfd7985693290b
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-12-01 20:59:51 -0300

    Corrige renderWithRouter repetido

--- src/tests/DoneRecipes.test.js
+++ src/tests/DoneRecipes.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import renderWithRouter from './renderWithRouter';
+import renderWithRouter from './helpers/renderWithRouter';
 import DoneRecipes from '../Pages/DoneRecipes';
 
 describe('Testa a página tela', () => {
--- src/tests/FavoriteRecipes.test.js
+++ src/tests/FavoriteRecipes.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import renderWithRouter from './renderWithRouter';
+import renderWithRouter from './helpers/renderWithRouter';
 import FavoriteRecipes from '../Pages/FavoriteRecipes';
 import Cards from '../components/Cards';
 
