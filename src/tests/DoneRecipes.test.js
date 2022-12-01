96c167a1355bad3ba9cde9c95ad16f40bb909661
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-12-01 17:47:55 -0300

    refatorando o código

--- src/tests/DoneRecipes.test.js
+++ src/tests/DoneRecipes.test.js
@@ -17,17 +17,4 @@ describe('Testa a página tela', () => {
     const drinksFooterImg = screen.getByTestId('drinks-bottom-btn');
     expect(drinksFooterImg).toBeInTheDocument();
   });
-
-  // test('teste se os botões de filtragem são renderizados', () => {
-  //   renderWithRouter(<DoneRecipes />);
-
-  //   const filterAllBtn = screen.getByTestId('filter-by-all-btn');
-  //   expect(filterAllBtn).toBeInTheDocument();
-
-  //   const filterMealBtn = screen.getByTestId('filter-by-meal-btn');
-  //   expect(filterMealBtn).toBeInTheDocument();
-
-  //   const filterDrinkBtn = screen.getByTestId('filter-by-drink-btn');
-  //   expect(filterDrinkBtn).toBeInTheDocument();
-  // });
 });
